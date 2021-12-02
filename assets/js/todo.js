6a3137c5f51008882931b0a6d5a76c7f34ac381e
Riya <9038riya@gmail.com>
2021-12-02 23:10:28 +0530

    final commit

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,15 +1,14 @@
 {
-    let id=$('#item2');
-    console.log(id);
-    
-    //  $( function() {
-    //      $( "#date" ).datepicker();
-    //    } );
- 
- 
-       // function to set background colour  of category 
- 
-    /*  let val= $('#item2').val();
+  let id = $("#item2");
+  console.log(id);
+
+  //  $( function() {
+  //      $( "#date" ).datepicker();
+  //    } );
+
+  // function to set background colour  of category
+
+  /*  let val= $('#item2').val();
       console.log('value of category is ',val);
         if(val=='work')
         {
@@ -18,32 +17,48 @@
           console.log(val);
           */
 
-       // jquery code to delete list which is checked
-       $("#del").on("click",function(){
-        $("input:checkbox").each(function() {
-            if ($(this).is(":checked")) {
-                $(this).parent().remove();
-                
-                let id=($(this).val());
-                $.ajax({
-                   method:'get',
-                   url:`/delete-contact/?id=${id}`,
-                   success:function(data)
-                   {
-                       console.log(data);
-                      // $(this).parent().remove();
-                   },error:function(err)
-                   {
-                       console.log(err);
-                   }
-               }
-           );
-         // $(this).parent().remove();
-           //var v=$(this).val();
-       }
-   });
- });
- 
-  
-       
- }
+  // jquery code to delete list which is checked
+  //        $("#del").on("click",function(){
+  //         $("input:checkbox").each(function() {
+  //             if ($(this).is(":checked")) {
+  //                 $(this).parentElement().remove();
+
+  //                 let id=($(this).val());
+  //                 $.ajax({
+  //                    method:'get',
+  //                    url:`/delete-contact/?id=${id}`,
+  //                    success:function(data)
+  //                    {
+  //                        console.log(data);
+  //                       // $(this).parent().remove();
+  //                    },error:function(err)
+  //                    {
+  //                        console.log(err);
+  //                    }
+  //                }
+  //            );
+  //          // $(this).parent().remove();
+  //            //var v=$(this).val();
+  //        }
+  //    });
+  //  });
+
+  const deleteEle = document.querySelector("#del");
+  deleteEle.addEventListener("click", deleteTodos);
+
+  async function deleteTodos() {
+    const checkedTodos = document.querySelectorAll("[type='checkbox']");
+    checkedTodos.forEach(async (checkedEle) => {
+      if (checkedEle.checked) {
+        checkedEle.parentElement.parentElement.parentElement.parentElement.remove();
+        let id = checkedEle.value;
+
+        const res = await fetch(`/delete-contact/?id=${id}`, {
+          method: "delete",
+        });
+        const data = await res.json();
+        console.log("res is", data);
+      }
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,75 +1,59 @@
-const express= require('express');
-const path=require('path');
-const port= 5000;
+const express = require("express");
+const path = require("path");
+const port = 5000;
 
-const db= require('./config/mongoose');
-const Todo = require('./models/todoList');
+const db = require("./config/mongoose");
+const Todo = require("./models/todoList");
 
-const app= express();
+const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded());
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 
-
-app.get('/',function(req,res)
-{
-    Todo.find({}, function(err,lists){
-        if(err)
-        {
-            console.log("error in fetching");
-            return;
-        }
-        return res.render('home',{
-            list:lists
-        })
-    })
+app.get("/", function (req, res) {
+  Todo.find({}, function (err, lists) {
+    if (err) {
+      console.log("error in fetching");
+      return;
+    }
+    return res.render("home", {
+      list: lists,
+    });
+  });
 });
 
 // CREATING TODO-LIST IN THE DATABASE
-app.post('/create-list', function(req,res){
-    // console.log("body",req.body)
-    Todo.create({
-        desc: req.body.desc,
-        category: req.body.category,
-        date: req.body.date
-    }, function(err, newList)
+app.post("/create-list", function (req, res) {
+  // console.log("body",req.body)
+  Todo.create(
     {
-        if(err)
-        {
-            console.log('error in creating list',err);
-            return;
-        }
-        console.log('***',newList);
-        return res.redirect('back');
-    })
-    
-})
+      desc: req.body.desc,
+      category: req.body.category,
+      date: req.body.date,
+    },
+    function (err, newList) {
+      if (err) {
+        console.log("error in creating list", err);
+        return;
+      }
+      return res.redirect("back");
+    }
+  );
+});
 
 // DELETING TODO-LIST FROM THE DATABASE
-app.get('/delete-contact', function(req,res){
-    if(req.xhr)
-    {
-        let id= req.query.id;
-        Todo.findByIdAndDelete(id, function(err)
-        {
-            if(err)
-            {
-                console.log("error in deleting",err);
-                return;
-            }
-            return res.redirect('back');
-        })
-    }
-})
-app.listen(port, function(err){
-    if(err)
-    {
-        console.log("error in listening");
-        return;
-    }
-    console.log("Server is running");
-})
-
+app.delete("/delete-contact", async function (req, res) {
+  let id = req.query.id;
+  const todo = await Todo.findByIdAndDelete(id);
+  return res.status(200);
+});
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error in listening");
+    return;
+  }
+  console.log("Server is running");
+});
